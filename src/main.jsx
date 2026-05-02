0c0ec82f54550de57698d4a121906a07b4899037
Siam <siam70344@gmail.com>
2026-05-02 10:25:56 +0600

    fix missing React imports in all components

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import "./index.css";
-import TimelineProvider from './context/TimelineContext.jsx';
-import { Toaster } from 'react-hot-toast';
+  import React from "react";
+  import ReactDOM from 'react-dom/client';
+  import App from './App';
+  import "./index.css";
+  import TimelineProvider from './context/TimelineContext.jsx';
+  import { Toaster } from 'react-hot-toast';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TimelineProvider>
-      <App />
-      <Toaster position="top-right" />
-    </TimelineProvider>
-  </React.StrictMode>,
-);
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <TimelineProvider>
+        <App />
+        <Toaster position="top-right" />
+      </TimelineProvider>
+    </React.StrictMode>,
+  );
